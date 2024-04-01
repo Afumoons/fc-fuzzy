4019eb8c48632eed00c6872438c734d8e3d8f1b0
Afu Sidhi <44629151+Afumoons@users.noreply.github.com>
2024-04-01 21:17:38 +0700

    Add isAdmin Gate on AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\User;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,12 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('isAdmin', function ($user) {
+            $user = User::with(['userRoles' => function ($userRoles) {
+                $userRoles->Admin();
+            }])->findOrFail($user->id);
+
+            return count($user->userRoles) != null ? true : false;
+        });
     }
 }
