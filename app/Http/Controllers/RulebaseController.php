248d25937d5c3554f5c4491e154096965e639d78
Afu Sidhi <44629151+Afumoons@users.noreply.github.com>
2024-04-01 20:57:44 +0700

    Create controller

---
+++ app/Http/Controllers/RulebaseController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rulebase;
+use App\Http\Requests\StoreRulebaseRequest;
+use App\Http\Requests\UpdateRulebaseRequest;
+
+class RulebaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRulebaseRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Rulebase $rulebase)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Rulebase $rulebase)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRulebaseRequest $request, Rulebase $rulebase)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Rulebase $rulebase)
+    {
+        //
+    }
+}
