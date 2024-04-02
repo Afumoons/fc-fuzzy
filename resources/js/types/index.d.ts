668199939106bb6e5d7cccab1be2d0c2e3a95507
Afu Sidhi <44629151+Afumoons@users.noreply.github.com>
2024-04-02 18:52:12 +0700

    Add new types for count on index.d.ts

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -5,8 +5,15 @@ export interface User {
     email_verified_at: string;
 }
 
+
+
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     auth: {
         user: User;
     };
+    symptomsCount: number;
+    diseasesCount: number;
+    usersCount: number;
+    adminsCount: number;
+    isAdmin: boolean;
 };
