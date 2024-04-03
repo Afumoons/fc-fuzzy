0d9b2004d8fbbada067092f92005e9fcf86f159c
Afu Sidhi <44629151+Afumoons@users.noreply.github.com>
2024-04-04 02:45:36 +0700

    New Dashboard.tsx content

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,8 +1,13 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import { PageProps } from "@/types";
+import DashboardCard from "@/Components/DashboardCard";
 
-export default function Dashboard({ auth }: PageProps) {
+export default function Dashboard({
+    auth,
+    isAdmin,
+    dashboardCounts,
+}: PageProps) {
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -11,14 +16,34 @@ export default function Dashboard({ auth }: PageProps) {
                     Dashboard
                 </h2>
             }
+            isAdmin={isAdmin}
         >
             <Head title="Dashboard" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
+                        <div className="row mb-3 p-3">
+                            <DashboardCard
+                                color="primary"
+                                count={dashboardCounts.symptomsCount}
+                                text="Jumlah Gejala"
+                            />
+                            <DashboardCard
+                                color="info"
+                                count={dashboardCounts.diseasesCount}
+                                text="Jumlah Penyakit"
+                            />
+                            <DashboardCard
+                                color="warning"
+                                count={dashboardCounts.usersCount}
+                                text="Jumlah Pengguna"
+                            />
+                            <DashboardCard
+                                color="danger"
+                                count={dashboardCounts.adminsCount}
+                                text="Jumlah Admin"
+                            />
                         </div>
                     </div>
                 </div>
