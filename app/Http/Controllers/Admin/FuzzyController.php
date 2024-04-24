75cf8a6413160009ae73b888508226deb804168c
Afu Sidhi <44629151+Afumoons@users.noreply.github.com>
2024-04-24 09:22:23 +0700

    Update FuzzyController

--- app/Http/Controllers/Admin/FuzzyController.php
+++ app/Http/Controllers/Admin/FuzzyController.php
@@ -2,199 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Disease;
 use App\Http\Controllers\Controller;
 
 class FuzzyController extends Controller
 {
-    function index()
-    {
-        // Data penyakit dan gejala (dibaca dari database)
-        $penyakit = [
-            "P1" => [
-                "gejala" => ["G1", "G2"],
-                "fungsiKeanggotaan" => [
-                    "G1" => [
-                        "Sedikit" => [0.5, 1.0, 1.5],
-                        "Sedang" => [1.0, 1.5, 2.0],
-                        "Banyak" => [1.5, 2.0, 2.5],
-                    ],
-                    "G2" => [
-                        "Sedikit" => [0.5, 1.0, 1.5],
-                        "Sedang" => [1.0, 1.5, 2.0],
-                        "Banyak" => [1.5, 2.0, 2.5],
-                    ],
-                ],
-                "aturan" => [
-                    [
-                        "antecedent" => ["G1" => "Sedikit", "G2" => "Sedikit"],
-                        "consequent" => ["Diagnosis" => "P1", "TingkatKeparahan" => 0.25]
-                    ],
-                    [
-                        "antecedent" => ["G1" => "Sedang", "G2" => "Sedang"],
-                        "consequent" => ["Diagnosis" => "P1", "TingkatKeparahan" => 0.5]
-                    ],
-                    [
-                        "antecedent" => ["G1" => "Banyak", "G2" => "Banyak"],
-                        "consequent" => ["Diagnosis" => "P1", "TingkatKeparahan" => 1.0]
-                    ]
-                ]
-            ],
-            "P2" => [
-                "gejala" => ["G2", "G3"],
-                "fungsiKeanggotaan" => [
-                    "G2" => [
-                        "Sedikit" => [0.5, 1.0, 1.5],
-                        "Sedang" => [1.0, 1.5, 2.0],
-                        "Banyak" => [1.5, 2.0, 2.5],
-                    ],
-                    "G3" => [
-                        "Sedikit" => [0.5, 1.0, 1.5],
-                        "Sedang" => [1.0, 1.5, 2.0],
-                        "Banyak" => [1.5, 2.0, 2.5],
-                    ]
-                ],
-                "aturan" => [
-                    [
-                        "antecedent" => ["G2" => "Sedikit", "G3" => "Sedikit"],
-                        "consequent" => ["Diagnosis" => "P2", "TingkatKeparahan" => 0.25]
-                    ],
-                    [
-                        "antecedent" => ["G2" => "Sedang", "G3" => "Sedang"],
-                        "consequent" => ["Diagnosis" => "P2", "TingkatKeparahan" => 0.5]
-                    ],
-                    [
-                        "antecedent" => ["G2" => "Banyak", "G3" => "Banyak"],
-                        "consequent" => ["Diagnosis" => "P2", "TingkatKeparahan" => 1.0]
-                    ]
-                ]
-            ],
-            "P3" => [
-                "gejala" => ["G1", "G3"],
-                "fungsiKeanggotaan" => [
-                    "G1" => [
-                        "Sedikit" => [0.5, 1.0, 1.5],
-                        "Sedang" => [1.0, 1.5, 2.0],
-                        "Banyak" => [1.5, 2.0, 2.5],
-                    ],
-                    "G3" => [
-                        "Sedikit" => [0.5, 1.0, 1.5],
-                        "Sedang" => [1.0, 1.5, 2.0],
-                        "Banyak" => [1.5, 2.0, 2.5],
-                    ]
-                ],
-                "aturan" => [
-                    [
-                        "antecedent" => ["G3" => "Sedikit", "G3" => "Sedikit"],
-                        "consequent" => ["Diagnosis" => "P3", "TingkatKeparahan" => 0.25]
-                    ],
-                    [
-                        "antecedent" => ["G3" => "Sedang", "G3" => "Sedang"],
-                        "consequent" => ["Diagnosis" => "P3", "TingkatKeparahan" => 0.5]
-                    ],
-                    [
-                        "antecedent" => ["G3" => "Banyak", "G3" => "Banyak"],
-                        "consequent" => ["Diagnosis" => "P3", "TingkatKeparahan" => 1.0]
-                    ]
-                ]
-            ],
-        ];
-
-        // Input dari pasien (diperoleh dari formulir web)
-        $inputPasien = [
-            "G1" => 1.5,
-            "G2" => 1.5,
-            "G3" => 0.5,
-        ];
-
-        dd($this->getAttributes(["G1", "G2", "G3", "G4", "G5", "G6", "G7"], ["1", "2", "3"]));
-
-        // Evaluasi aturan Fuzzy Sugeno
-        $aktivasiAturan = [];
-        foreach ($penyakit as $namaPenyakit => $dataPenyakit) {
-            $aktivasiAturan[$namaPenyakit] = [];
-            foreach ($dataPenyakit["aturan"] as $aturan) {
-                $derajatAktivasi = 1.0;
-                foreach ($aturan["antecedent"] as $gejala => $kategori) {
-                    $derajatKeanggotaanGejala = $this->derajatKeanggotaan($inputPasien[$gejala], $dataPenyakit["fungsiKeanggotaan"][$gejala], $kategori);
-                    $derajatAktivasi = min($derajatAktivasi, $derajatKeanggotaanGejala);
-                }
-                $aktivasiAturan[$namaPenyakit][] = $derajatAktivasi * $aturan["consequent"]["TingkatKeparahan"];
-            }
-        }
-
-        // Agregasi dan defuzzifikasi
-        $diagnosis = "";
-        $tingkatKeparahanTertinggi = 0.0;
-        foreach ($aktivasiAturan as $namaPenyakit => $aktivasiPenyakit) {
-            $totalAktivasi = 0.0;
-            $bobotTotal = 0.0;
-            foreach ($aktivasiPenyakit as $aktivasiAturan) {
-                $totalAktivasi += $aktivasiAturan;
-                $bobotTotal += 1.0;
-            }
-
-            if ($bobotTotal > 0.0) {
-                $tingkatKeparahan = $totalAktivasi / $bobotTotal;
-                if ($tingkatKeparahan > $tingkatKeparahanTertinggi) {
-                    $diagnosis = $namaPenyakit;
-                    $tingkatKeparahanTertinggi = $tingkatKeparahan;
-                }
-            }
-        }
-
-        // dd($this->getAttribute(['G1', 'G2'], ['Sedikit', 'Sedang', 'Banyak']));
-
-        // Hasil diagnosis
-        if ($diagnosis != "") {
-            echo "Diagnosis: $diagnosis\n";
-            echo "Tingkat Keparahan: $tingkatKeparahanTertinggi\n";
-        } else {
-            echo "Diagnosis tidak dapat ditentukan.\n";
-        }
-    }
-
-    /**
-     * This function calculates the membership degree.
-     * @param float $nilai The input value.
-     * @param array $fungsiKeanggotaan The membership function.
-     * @param string $kategori The category of the input value.
-     * @return float The membership degree.
-     */
-    function derajatKeanggotaan($nilai, $fungsiKeanggotaan, $kategori)
-    {
-        if ($kategori == "Sedikit") {
-            list($a1, $a2, $a3) = $fungsiKeanggotaan[$kategori];
-            if ($nilai < $a1) {
-                return 1.0;
-            } else if ($nilai <= $a2) {
-                return ($a3 - $nilai) / ($a3 - $a2);
-            } else {
-                return 0.0;
-            }
-        } else if ($kategori == "Sedang") {
-            list($a1, $a2, $a3) = $fungsiKeanggotaan[$kategori];
-            if ($nilai < $a1) {
-                return 0.0;
-            } else if ($nilai <= $a2) {
-                return ($nilai - $a1) / ($a2 - $a1);
-            } else {
-                return ($a3 - $nilai) / ($a3 - $a2);
-            }
-        } else if ($kategori == "Banyak") {
-            list($a1, $a2, $a3) = $fungsiKeanggotaan[$kategori];
-            if ($nilai < $a1) {
-                return 0.0;
-            } else if ($nilai <= $a2) {
-                return ($nilai - $a1) / ($a2 - $a1);
-            } else if ($nilai <= $a3) {
-                return 1.0;
-            } else {
-                return 0.0;
-            }
-        }
-    }
-
-    function getAttributes($hurufs, $angkas)
+    private function getAttributes($hurufs, $angkas)
     {
         $huruf = [];
         $angka = [];
@@ -221,13 +34,13 @@ class FuzzyController extends Controller
                 $churuf++;
                 $duar = $temp[$churuf];
 
-                echo "$huruf[$churuf]";
-                echo " => ";
-                echo "$angka[$duar]";
-                echo "\n";
+                // echo "$huruf[$churuf]";
+                // echo " => ";
+                // echo "$angka[$duar]";
+                // echo "\n";
                 $data[$huruf[$churuf]] = $angka[$duar];
             }
-            echo " <br> ";
+            // echo " <br> ";
             $dataPembungkus[$countDataPembungkus] = $data;
             $countDataPembungkus++;
 
@@ -245,14 +58,14 @@ class FuzzyController extends Controller
         return $dataPembungkus;
     }
 
-    function getRuleAttributes($symptoms, $statements, $disease)
+    private function getRuleAttributes($symptoms, $statements, $disease)
     {
         $huruf = [];
         $angka = [];
         $symptomTotal = count($symptoms);
         $statementTotal = count($statements);
         foreach ($symptoms as $key => $symptom) {
-            $huruf[$key + 1] = $symptom->code;
+            $huruf[$key + 1] = $symptom;
         }
         foreach ($statements as $key => $statement) {
             $angka[$key + 1] = $statement;
@@ -271,9 +84,9 @@ class FuzzyController extends Controller
             $dataAntecedent = [];
             $dataConsequent = [];
             $tingkatKeparahan = 0;
-            $nilaiBanyak = 100 / $symptomTotal;
-            $nilaiSedang = 100 / $symptomTotal / $statementTotal * 2;
-            $nilaiSedikit = 100 / $symptomTotal / $statementTotal;
+            $nilaiBanyak = (90) / $symptomTotal;
+            $nilaiSedang = (90) / $symptomTotal / $statementTotal * 2;
+            $nilaiSedikit = (90) / $symptomTotal / $statementTotal;
             while ($churuf < $symptomTotal) {
                 $churuf++;
                 $duar = $temp[$churuf];
@@ -313,5 +126,137 @@ class FuzzyController extends Controller
             }
         }
         return $dataPembungkus;
+    }
+
+    function index()
+    {
+        $disease = Disease::first();
+        $rulebases = $disease->rulebases()->where('value', true)->get();
+        $symptomsArray = [];
+        $membershipFunctionArray = [];
+        $statementsArray = ['Sedikit', 'Sedang', 'Banyak'];
+        foreach ($rulebases as $rulebaseKey => $rulebase) {
+            $angka = 0.0;
+            $angka2 = 0.5;
+            $angka3 = 1.0;
+            $symptomsArray[$rulebaseKey] = $rulebase->symptom->code;
+            foreach ($statementsArray as  $statement) {
+                $membershipFunctionArray[$rulebase->symptom->code][$statement] = [$angka, $angka2, $angka3];
+                $angka = $angka + 0.5;
+                $angka2 = $angka2 + 0.5;
+                $angka3 = $angka3 + 0.5;
+            }
+        }
+        // Data penyakit dan gejala (dibaca dari database)
+        $penyakit = [
+            "P1" => [
+                "gejala" => $symptomsArray,
+                "fungsiKeanggotaan" => $membershipFunctionArray,
+                "aturan" => $this->getRuleAttributes($symptomsArray, $statementsArray, $rulebases[0]->disease->code)
+            ],
+        ];
+
+        // Input dari pasien (diperoleh dari formulir web)
+        $inputPasien = [
+            "G1" => 1.5,
+            "G2" => 1.5,
+            "G3" => 1.5,
+            "G4" => 1.5,
+            "G5" => 1.5,
+            "G6" => 1.5,
+            "G7" => 1.5,
+        ];
+
+        // Evaluasi aturan Fuzzy Sugeno
+        $aktivasiAturan = [];
+        foreach ($penyakit as $namaPenyakit => $dataPenyakit) {
+            $aktivasiAturan[$namaPenyakit] = [];
+            foreach ($dataPenyakit["aturan"] as $aturan) {
+                $derajatAktivasi = 1.0;
+                foreach ($aturan["antecedent"] as $gejala => $kategori) {
+                    $derajatKeanggotaanGejala = $this->derajatKeanggotaan($inputPasien[$gejala], $dataPenyakit["fungsiKeanggotaan"][$gejala], $kategori);
+                    $derajatAktivasi = min($derajatAktivasi, $derajatKeanggotaanGejala);
+                }
+                // dd($derajatKeanggotaanGejala, $derajatAktivasi);
+                $aktivasiAturan[$namaPenyakit][] = $derajatAktivasi * $aturan["consequent"]["TingkatKeparahan"];
+            }
+        }
+
+        // Agregasi dan defuzzifikasi
+        $diagnosis = "";
+        $tingkatKeparahanTertinggi = 0.0;
+        foreach ($aktivasiAturan as $namaPenyakit => $aktivasiPenyakit) {
+            $totalAktivasi = 0.0;
+            $bobotTotal = 0.0;
+            foreach ($aktivasiPenyakit as $aktivasiAturan2) {
+                $totalAktivasi += $aktivasiAturan2;
+                $bobotTotal += 1.0;
+            }
+
+            if ($bobotTotal > 0.0) {
+                $tingkatKeparahan = $totalAktivasi / $bobotTotal;
+                if ($tingkatKeparahan > $tingkatKeparahanTertinggi) {
+                    $diagnosis = $namaPenyakit;
+                    $tingkatKeparahanTertinggi = $tingkatKeparahan;
+                }
+            }
+        }
+        $tingkatKeparahanTertinggi = $tingkatKeparahanTertinggi * $bobotTotal;
+
+        // dd($this->getAttribute(['G1', 'G2'], ['Sedikit', 'Sedang', 'Banyak']));
+
+        // Hasil diagnosis
+        if ($diagnosis != "") {
+            echo "Diagnosis: $diagnosis\n";
+            echo "Tingkat Keparahan: $tingkatKeparahanTertinggi\n";
+        } else {
+            echo "Diagnosis tidak dapat ditentukan.\n";
+        }
+        dd($penyakit);
+    }
+
+    /**
+     * This function calculates the membership degree.
+     * @param float $nilai The input value.
+     * @param array $fungsiKeanggotaan The membership function.
+     * @param string $kategori The category of the input value.
+     * @return float The membership degree.
+     */
+    function derajatKeanggotaan($nilai, $fungsiKeanggotaan, $kategori)
+    {
+        if ($kategori == "Sedikit") {
+            list($a1, $a2, $a3) = $fungsiKeanggotaan[$kategori];
+            if ($nilai < $a1) { // $nilai < 0
+                return 1.0;
+            } else if ($nilai <= $a2) { // $nilai <= 0.5
+                // 1.0 - $nilai / 1.0 - 0.5
+                return ($a3 - $nilai) / ($a3 - $a2);
+            } else {
+                return 0.0;
+            }
+        } else if ($kategori == "Sedang") {
+            list($a1, $a2, $a3) = $fungsiKeanggotaan[$kategori];
+            if ($nilai < $a1) { // $nilai < 0.5
+                return 0.0;
+            } else if ($nilai <= $a2) { // $nilai <= 1.0
+                // ($nilai - 0.5) / (1.0 - 0.5)
+                return ($nilai - $a1) / ($a2 - $a1);
+            } else {
+                // (1.5 - $nilai) / (1.5 - 1.0)
+                return ($a3 - $nilai) / ($a3 - $a2);
+            }
+        } else if ($kategori == "Banyak") {
+            list($a1, $a2, $a3) = $fungsiKeanggotaan[$kategori];
+            if ($nilai < $a1) { // $nilai < 1.0
+                return 0.0;
+            } else if ($nilai <= $a2) { // $nilai <= 1.5
+                // ($nilai - 1.0) / (1.5 - 1.0)
+                return ($nilai - $a1) / ($a2 - $a1);
+            } else if ($nilai <= $a3) { // $nilai <= 2
+                return 1.0;
+            } else {
+                return 0.0;
+            }
+        }
     }
 }
