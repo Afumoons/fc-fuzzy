9b36f92cb015d48385919a69ae4789437860013a
Afu Sidhi <44629151+Afumoons@users.noreply.github.com>
2024-04-28 13:56:29 +0700

    Add exception for null disease

--- resources/js/Pages/Diagnosis.tsx
+++ resources/js/Pages/Diagnosis.tsx
@@ -123,7 +123,11 @@ export default function Diagnosis({
                                         diderita dengan menggunakan fuzzy
                                     </p>
                                     <Link
-                                        href={route("fuzzy", [disease.code])}
+                                        href={
+                                            disease
+                                                ? route("fuzzy", [disease.code])
+                                                : "#"
+                                        }
                                         className="d-block w-25 text-center inline-flex items-center px-4 py-2 bg-blue-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-600 focus:bg-blue-600 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
                                     >
                                         Cek sekarang
