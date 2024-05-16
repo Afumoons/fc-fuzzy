745cf3ca67ce345eca430117ddbadece20ac0277
Afu Sidhi <44629151+Afumoons@users.noreply.github.com>
2024-05-16 12:18:52 +0700

    Remove unused comment/codes on admin_routes/rulebaseHistory

--- routes/admin_routes/rulebaseHistory.php
+++ routes/admin_routes/rulebaseHistory.php
@@ -4,10 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\RulebaseHistoryController;
 
 // admin.rulebaseHistory -> admin/rulebaseHistory
-// Route::get('/', [RulebaseHistoryController::class, 'index'])->name('index');
-// Route::get('/dt/conducting-inspection/ajax', [RulebaseHistoryController::class, 'DtConductingInspectionList'])->name('dt.conducting-inspection.ajax');
-// Route::get('/create', [RulebaseHistoryController::class, 'create'])->name('create');
-// Route::post('/store', [RulebaseHistoryController::class, 'store'])->name('store');
-// Route::get('/{rulebaseHistory}', [RulebaseHistoryController::class, 'edit'])->name('edit');
-// Route::post('/{rulebaseHistory}', [RulebaseHistoryController::class, 'update'])->name('update');
 Route::get('/destroy/{rulebaseHistory}', [RulebaseHistoryController::class, 'destroy'])->name('destroy');
